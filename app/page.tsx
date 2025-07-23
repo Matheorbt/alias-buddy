7d7e8c4b2b31db44b5b3bf224259810696567a8f
matheorbt <matho.robert@gmail.com>
2025-07-23 23:22:19 +0200

    add test feature flag

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,10 @@ export default function Home() {
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1>Hello World</h1>
       <button onClick={() => posthog.capture("button_clicked")}>Click me</button>
+      {posthog.isFeatureEnabled("welcome-banner") &&
+      <div>Welcome to the app !</div>
+      }
     </div>
   );
 }
